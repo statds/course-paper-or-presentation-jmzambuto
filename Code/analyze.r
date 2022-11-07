8c4ad852f6c2dbd82b43feba9a1ed19c5f264667
Jessica Zambuto <jessica.zambuto@uconn.edu>
2022-11-07 17:14:39 -0500

    added R

---
+++ Code/analyze.r
@@ -0,0 +1,29 @@
+priorcovidtoduring<-prop.test(c(227738.182,227934.374),n=c(439649,437494),conf.level = .95,correct=FALSE)
+priorcovidtoduring
+
+duringcovidtoafter<-prop.test(c(227934.374,231292.29),n=c(437494,449985),conf.level = .95,correct=FALSE)
+duringcovidtoafter
+
+priorcovid617<-prop.test(c(97800.521,85428.838),n=c(153533,145783),conf.level = .95,correct=FALSE)
+priorcovid617
+
+duringcovid617<-prop.test(c(85428.838,91408.388),n=c(145783,158146),conf.level = .95,correct=FALSE)
+duringcovid617
+
+priorcovid1849<-prop.test(c(38421.888,40406.86),n=c(100057,107180),conf.level = .95,correct=FALSE)
+priorcovid1849
+
+duringcovid1849<-prop.test(c(40406.86,39203.941),n=c(107180,105671),conf.level = .95,correct=FALSE)
+duringcovid1849
+
+priorcovid5064 <-prop.test(c(40375.764,54288.7),n=c(79794,79128),conf.level = .95,correct=FALSE)
+priorcovid5064
+
+duringcovid5064 <-prop.test(c(54288.7,40579.608),n=c(79128,77442),conf.level = .95,correct=FALSE)
+duringcovid5064
+
+priorcovid65 <-prop.test(c(74172.97,79263.056),n=c(106265,105403),conf.level = .95,correct=FALSE)
+priorcovid65
+
+duringcovid65 <-prop.test(c(79263.056,80348.514),n=c(105403,108726),conf.level = .95,correct=FALSE)
+duringcovid65
